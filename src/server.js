28cdc096870d7220143654470c45a894eb41a757
01mccoy <unogle12@gmail.com>
2022-08-24 19:57:40 +0900

    #2. Router & Controller

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 7000;
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`Server listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
