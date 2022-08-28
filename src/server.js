1f4e96585fa376a41713a073b69f793853685281
01mccoy <unogle12@gmail.com>
2022-08-29 02:34:22 +0900

    #3-3. HTML Rendering with Nunjucks

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import express from "express";
+import nunjucks from "nunjucks";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug");
+app.set("view engine", "html");
 app.set("views", process.cwd() + "/src/views");
+
+nunjucks.configure(app.get("views"), {
+  autoescape: true,
+  express: app,
+  watch: true,
+});
 
 app.use(logger);
 
