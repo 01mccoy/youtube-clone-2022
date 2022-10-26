630b978500c7247edcdaaf85463e605d44a6565b
01mccoy <unogle12@gmail.com>
2022-10-26 12:30:57 +0900

    #6. Connect to Mongo

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
 
@@ -27,7 +25,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
