b412295af9318b18ae39b329ac914c1ccf69dfbb
01mccoy <unogle12@gmail.com>
2022-11-15 14:28:42 +0900

    #11. Bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -8,6 +9,10 @@ const userSchema = new mongoose.Schema({
   location: String,
 });
 
-const User = mongoose.Model("User", userSchema);
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
+});
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
